00413640ad4c79f10fa2a8eb1612c066eec946c5
nehathumu <nthumu@haverford.edu>
2023-02-17 22:28:29 -0500

    quad

--- src/draw_art.cpp
+++ src/draw_art.cpp
@@ -6,18 +6,6 @@ using namespace agl;
 int main(int argc, char** argv)
 {
    Canvas drawer(640, 380);
-   drawer.background(0, 0, 0);
-   drawer.begin(TRIANGLES);
-   drawer.color(255, 0, 255);
-   drawer.vertex(10, 10);
-   drawer.vertex(10, 90);
-   drawer.vertex(90, 90);
-   drawer.color(255, 255, 0);
-   drawer.vertex(90, 90);
-   drawer.vertex(90, 10);
-   drawer.vertex(10, 10);
-   drawer.end();
-   drawer.save("test.png");
    // your code here
 }
 
